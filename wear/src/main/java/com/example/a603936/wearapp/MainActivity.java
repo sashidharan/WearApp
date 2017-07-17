068ea6d6f959078ab0474cd7cda783e1fbb80d86
sashidharan <sashi.it4@gmail.com>
2017-07-17 20:33:16 +0530

    inital commit

---
+++ wear/src/main/java/com/example/a603936/wearapp/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.a603936.wearapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+
+    public void feedList(View v) {
+        addListenerButton();
+    }
+
+    public void addListenerButton() {
+        final Context context = this;
+        Toast.makeText(getApplicationContext(), "You Came to Feed List", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(context, FeedActivity.class);
+        startActivity(intent);
+        /*button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, FeedActivity.class);
+                startActivity(intent);
+            }
+        });*/
+    }
+
+
+
+}
